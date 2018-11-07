14de478a4a6911a58a8e31c512630a0b60cf36cb
SergeyGluxov <gluxov-serega@mail.ru>
2018-11-08 00:31:38 +0700

    Внес изменения в мастер

--- app/src/androidTest/java/info/goodline/cardbag/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/info/goodline/cardbag/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.homework3;
+package info.goodline.cardbag;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.homework3", appContext.getPackageName());
+        assertEquals("info.goodline.cardbag", appContext.getPackageName());
     }
 }
